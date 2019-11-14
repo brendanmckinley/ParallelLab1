a5d87ed4c1aba9b87902bdc8513fa459536f95ca
Brendan <brendan.mckinley@eagles.oc.edu>
2019-11-13 19:34:49 -0600

    First commit (initialize project)

---
+++ cmake-build-debug/CMakeFiles/Lab1_MonteCarlo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1_MonteCarlo.dir/main.c.o"
+  "Lab1_MonteCarlo"
+  "Lab1_MonteCarlo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1_MonteCarlo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
